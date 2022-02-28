ae4fc72b1422113c2135668c997378c61fbe0e49
arminjoya <94761705+armin1442@users.noreply.github.com>
2022-02-28 14:09:44 +0330

    Add files via upload

---
+++ tast4.py
@@ -0,0 +1,15 @@
+from re import X
+
+
+def moadele_daraje_yek(y,m,b):
+    x=(y-m)  /b
+    print(X)
+
+moadele_daraje_yek(9,5,4)
+moadele_daraje_yek(8,5,6)
+moadele_daraje_yek(9,7,2)
+moadele_daraje_yek(4,6,4)
+moadele_daraje_yek(6,3,2)
+
+
+
---
+++ tast44.py
@@ -0,0 +1,9 @@
+from tkinter.tix import InputOnly
+
+
+o = int(input())
+n = int(input())
+m = int(input())
+
+x = (o-n) /m
+print(x)
