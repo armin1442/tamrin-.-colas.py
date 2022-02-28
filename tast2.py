5a206d82ec8e98aed3fd63fe65212205fa6cf99d
arminjoya <94761705+armin1442@users.noreply.github.com>
2022-02-28 14:08:30 +0330

    Add files via upload

---
+++ tast2.py
@@ -0,0 +1,19 @@
+
+print ("1-welcome")
+print("2-n")
+print("3-c")
+print("4-div")
+print('5-rin')
+
+op = input ()
+
+a = int(input())
+d = int(input())
+
+if op == "+":
+    result = a + d
+
+elif op == "-":
+    result = a - d
+
+print(result)    
---
+++ tast3.py
@@ -0,0 +1,7 @@
+def Khoshamd_gooye():
+    name = input()
+    print("welcome",name)
+
+ Khoshamd_gooye()
+ Khoshamd_gooye()
+ Khoshamd_gooye()
